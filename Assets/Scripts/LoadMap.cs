59e2f0ebed9868a68b196f1eb2171ba3dfcabd91
lew0205 <lew0205@naver.com>
2021-12-15 20:24:21 +0900

    21/12/15 json을 통해 맵을 불러올 준비

---
+++ Assets/Scripts/LoadMap.cs
@@ -0,0 +1,18 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class LoadMap : MonoBehaviour
+//{
+//    public mapData;
+
+//    void Awake()
+//    {
+
+//    }
+
+//    void loadMap()
+//    {
+//        string jsonData = JsonUtility.ToJson(player)
+//    }
+//}
